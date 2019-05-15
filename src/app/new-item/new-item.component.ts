c3f62c42e0e40d98695f4bb9c204e49ec1e9c4e8
Tiwari <sanjeet.tiwari@siemens.com>
2019-05-16 00:08:12 +0530

    Added fields in Add New Item Page

--- src/app/new-item/new-item.component.ts
+++ src/app/new-item/new-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-new-item',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewItemComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('yesbut') yesButton: ElementRef;
+  @ViewChild('nobut') noButton: ElementRef;
 
-  ngOnInit() {
+  constructor(private renderer: Renderer2) { }
+
+  ngOnInit() { }
+
+  isEmiThere(status: boolean) {
+    
   }
 
 }
